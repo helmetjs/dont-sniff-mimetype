8d212eefa2fae662dde20a2353325ba1e185683e
Evan Hahn <me@evanhahn.com>
2015-12-18 14:17:08 -0800

    Use Standard.js linting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-module.exports = function nosniff() {
-  return function nosniff(req, res, next) {
-    res.setHeader('X-Content-Type-Options', 'nosniff');
-    next();
-  };
-};
+module.exports = function nosniff () {
+  return function nosniff (req, res, next) {
+    res.setHeader('X-Content-Type-Options', 'nosniff')
+    next()
+  }
+}
--- test/index.js
+++ test/index.js
@@ -1,28 +1,26 @@
-var nosniff = require('..');
+var nosniff = require('..')
 
-var assert = require('assert');
-var connect = require('connect');
-var request = require('supertest');
+var assert = require('assert')
+var connect = require('connect')
+var request = require('supertest')
 
 describe('nosniff', function () {
-
-  var app;
+  var app
   beforeEach(function () {
-    app = connect();
-    app.use(nosniff());
+    app = connect()
+    app.use(nosniff())
     app.use(function (req, res) {
-      res.end('Hello world!');
-    });
-  });
+      res.end('Hello world!')
+    })
+  })
 
   it('sets header properly', function (done) {
     request(app).get('/')
-    .expect('X-Content-Type-Options', 'nosniff', done);
-  });
+      .expect('X-Content-Type-Options', 'nosniff', done)
+  })
 
   it('names its function and middleware', function () {
-    assert.equal(nosniff.name, 'nosniff');
-    assert.equal(nosniff().name, 'nosniff');
-  });
-
-});
+    assert.equal(nosniff.name, 'nosniff')
+    assert.equal(nosniff().name, 'nosniff')
+  })
+})
