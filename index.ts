0312edff77a4ae5fce535ae12aa6ec93c03198a7
Evan Hahn <me@evanhahn.com>
2020-06-29 13:54:50 -0500

    Mark this module as having moved to helmetjs/helmet

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { IncomingMessage, ServerResponse } from 'http';
-
-export = function dontSniffMimetype () {
-  return function dontSniffMimetype (_req: IncomingMessage, res: ServerResponse, next: () => void) {
-    res.setHeader('X-Content-Type-Options', 'nosniff');
-    next();
-  };
-};
